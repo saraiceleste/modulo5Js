860b0813d42684ee6ba77419ce70313faa777a5e
Sarai <saraimontes170@gmail.com>
2026-05-22 13:12:38 -0600

    add project espacial

---
+++ clase7Proyecto/index.js
@@ -0,0 +1,9 @@
+const planetas = require('./planetas');
+// Aquí mostraremos la información de los planetas
+
+planetas.forEach(planeta => {
+  console.log(`¡Planeta ${planeta.nombre} descubierto!`);
+  console.log(`Descripción: ${planeta.descripcion}`);
+  console.log(`Descubierto en: ${planeta.descubiertoEn}`);
+  console.log(`Dato curioso: ${planeta.datoCurioso}`);
+});
---
+++ clase7Proyecto/planetas.js
@@ -0,0 +1,28 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655",
+    datoCurioso: "Titán es el único satélite natural conocido que tiene una atmósfera densa."
+  }, 
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016",
+    datoCurioso: "Próxima Centauri b es el exoplaneta más cercano a la Tierra, a solo 4.24 años luz de distancia."
+  }, 
+  {
+    nombre: "Kepler-22b",
+    descripcion: "Un exoplaneta que podría tener océanos de agua líquida.",
+    descubiertoEn: "2011",
+    datoCurioso: "Kepler-22b es uno de los primeros exoplanetas descubiertos en la zona habitable de su estrella."
+  }, 
+  {
+    nombre: "la Tierra",
+    descripcion: "Nuestro hogar, el único planeta conocido con vida.",
+    descubiertoEn: "Antigüedad",
+    datoCurioso: "La Tierra es el único planeta conocido que alberga vida."
+  } 
+];
+
+module.exports = planetas;
